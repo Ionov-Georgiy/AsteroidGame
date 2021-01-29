2b1e1a188784dcd7daf21dbe77e735fef36ccbaf
Ionov-Georgiy <74145892+Ionov-Georgiy@users.noreply.github.com>
2021-01-29 17:48:55 +0600

    Add files via upload

---
+++ AsteroidGame/AsteroidGame/Objects/EnergyBar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsteroidGame.Interfaces;
+
+namespace AsteroidGame.Objects
+{
+    class EnergyBar : IUIElement
+    {
+        public Size ElementSize => elementSize;
+        private Size elementSize = new Size(100, 10);
+        public int CurrentEnergy;
+        public int MaxEnergy;
+
+        public EnergyBar(int CurrentEnergy, int MaxEnergy)
+        {
+            this.CurrentEnergy = CurrentEnergy;
+            this.MaxEnergy = MaxEnergy;
+        }
+
+        public void Draw(Graphics g, Point ElementLocation)
+        {
+            g.DrawRectangle(Pens.Red, ElementLocation.X, ElementLocation.Y, ElementSize.Width, ElementSize.Height);
+            g.FillRectangle(Brushes.Red, ElementLocation.X, ElementLocation.Y, CurrentEnergy*ElementSize.Width/MaxEnergy, ElementSize.Height);
+        }
+
+    }
+}
---
+++ AsteroidGame/AsteroidGame/Objects/UI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsteroidGame.Interfaces;
+
+namespace AsteroidGame.Objects
+{
+    class UI
+    {
+        public readonly Point Location;
+        public Size UISize => uiSize;
+        private Size uiSize = Size.Empty;
+        public Dictionary<IUIElement, Point> UIElements = new Dictionary<IUIElement, Point>();
+
+        public UI(Point Location, IEnumerable<IUIElement>UIElementsList)
+        {
+            this.Location = Location;
+            foreach (IUIElement elem in UIElementsList)
+            {
+                UIElements.Add(elem, new Point(Location.X + UISize.Width, Location.Y));
+                uiSize.Width += elem.ElementSize.Width + 20;
+            }            
+        }
+
+        public void Draw(Graphics g)
+        {
+            //UIElements.Select(
+            //    (KeyValuePair<IUIElement, Point> el) => { 
+            //        el.Key.Draw(g, el.Value); 
+            //        return el;
+            //    });
+        }
+    }
+}
